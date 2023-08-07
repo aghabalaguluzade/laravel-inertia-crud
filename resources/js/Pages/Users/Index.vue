40dee3b3ec9358655b0e3e53722684afbb90527d
aghabalaguluzade <agabala.oyunda@gmail.com>
2023-08-07 17:00:00 +0400

    better performans debounce

--- resources/js/Pages/Users/Index.vue
+++ resources/js/Pages/Users/Index.vue
@@ -50,7 +50,7 @@
     import Pagination from "../../Shared/Pagination.vue";
     import { ref, watch } from "vue";
     import { router } from '@inertiajs/vue3'
-
+    import debounce from "lodash/debounce";
 
     let props = defineProps({
         users : Object,
@@ -59,10 +59,10 @@
 
     let search = ref(props.filters.search);
 
-    watch(search,value => {
+    watch(search, debounce(function(value) {
         router.get('/users', { search : value }, {
             preserveState : true,
             replace : true
-        })
+        }, 300);
     });
 </script>
