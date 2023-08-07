80b3f6b8955c10f7dbc51ac745daac169e604f4d
aghabalaguluzade <agabala.oyunda@gmail.com>
2023-08-07 16:20:49 +0400

    show validation errors

--- resources/js/Pages/Users/Create.vue
+++ resources/js/Pages/Users/Create.vue
@@ -9,16 +9,19 @@
         <div class="mb-6">
             <label for="name" class="block mb-2 uppercase font-bold text-xs text-gray-700">Name</label>
             <input type="text" name="name" id="name" class="border border-gray-400 p-2 w-full" v-model="form.name" />
+            <div v-if="errors.name" v-text="errors.name" class="text-red-500 text-xs mt-1"></div>
         </div>
 
         <div class="mb-6">
             <label for="email" class="block mb-2 uppercase font-bold text-xs text-gray-700">Email</label>
             <input type="email" name="email" id="email" class="border border-gray-400 p-2 w-full" v-model="form.email" />
+            <div v-if="errors.email" v-text="errors.email" class="text-red-500 text-xs mt-1"></div>
         </div>
 
         <div class="mb-6">
             <label for="password" class="block mb-2 uppercase font-bold text-xs text-gray-700">Password</label>
             <input type="password" name="password" id="password" class="border border-gray-400 p-2 w-full" v-model="form.password" />
+            <div v-if="errors.password" v-text="errors.password" class="text-red-500 text-xs mt-1"></div>
         </div>
 
         <div class="mb-6">
@@ -29,16 +32,20 @@
 </template>
 
 <script setup>
-    import {reactive} from "vue";
+import {reactive, ref} from "vue";
     import { router } from '@inertiajs/vue3'
+
+    defineProps({
+        errors : Object
+    });
 
     let form = reactive({
         name : "",
         email : "",
         password : ""
-    })
+    });
 
     let submit = () => {
         router.post("/users", form);
-    }
+    };
 </script>
